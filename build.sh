35a0c66271572913391cda699c0354d5d8f9c8e1
Scott Beamer <sbeamer@eecs.berkeley.edu>
2015-08-19 17:37:06 -0700

    try separate again

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo "Starting RISC-V Toolchain build process"
 build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
 build_project riscv-gnu-toolchain --prefix=$RISCV
-build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --build=riscv64-unknown-elf
-build_tests
+# build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --build=riscv64-unknown-elf
+# build_tests
 
 echo -e "\\nRISC-V Toolchain installation completed!"
